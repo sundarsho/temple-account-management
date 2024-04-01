124e4b52147e880e398b4a37061eefc720fca811
sundarsho <98793671+sundarsho@users.noreply.github.com>
2024-03-31 23:50:25 -0500

    Home Screen Changes

--- temple-account-ui-client/src/app/app-routing.module.ts
+++ temple-account-ui-client/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './pages/user/user.component';
 import { UserlistComponent } from './pages/userlist/userlist.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {path:'userlist',component: UserlistComponent},
-  {path:'user',component: UserComponent}
-  // {path:'slider',component:SliderComponent},
+  {path:'user',component: UserComponent},
+  {path:'home',component:HomeComponent}
   // {path:'table',component:TableComponent},
   // {path:'form',component:FormdesignComponent},
   // {path:'associate',component:AssociateComponent}
